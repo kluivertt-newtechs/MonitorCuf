249674aff3d2de43da5cb61aadf6ff0364037c3f
kluivertt-araujo <kluivertt.araujo@newtechs.com.br>
2024-09-17 21:47:35 -0300

    ajuste menu

--- src/app/main/monitores/monitores-routing.module.ts
+++ src/app/main/monitores/monitores-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
     path: '',
     component: MonitoresComponent,
     children: [
-      { path: '', pathMatch: 'full', redirectTo: 'cuf0069' },
+      { path: '', pathMatch: 'full', redirectTo: '' },
       { path: 'cuf0069', component: Cuf0069Component },
     ],
   },
